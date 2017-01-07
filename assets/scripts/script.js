7706d4fbb2e8042613903f06897e8789cedaaaf2
bazfer <bazfer@gmail.com>
2017-01-07 07:26:28 -0600

    heroku

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -95,7 +95,7 @@ function getSolarTimes() {
 
 	timezoneURL = "https://maps.googleapis.com/maps/api/timezone/json?location=" + latitude + "," + longitude + "&timestamp=" + timestamp + "&key=" + googleAPIKey;
 
-	solarQueryURL = "http://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + date;
+	solarQueryURL = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + date;
 
 	$.ajax({ url: timezoneURL, method: "GET" }).done(function(response) {
 
